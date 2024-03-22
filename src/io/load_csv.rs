a589e80ee875a6479c8f10ef11a1a5b8a8ea3afc
Andrew Jensen <andrewjensen321@gmail.com>
2024-03-22 13:33:55 -0400

    Loading matrix into nalgebra (`from_fn()`). Returning matrix in `main()`. Matrix size known only at runtime.

--- src/io/load_csv.rs
+++ src/io/load_csv.rs
@@ -1,18 +1,41 @@
 use std::env;
 use std::fs;
 
-pub fn print_string_contents(fp: &str) {
+extern crate nalgebra as na;
+extern crate ndarray as np;
+use na::*;
+pub fn print_string_contents(fp: &str) -> DMatrix<f64> {
     println!("We are in: {:?}", env::current_dir());
     println!("File Name {}", fp);
     let contents = fs::read_to_string(fp).expect("We should have been able to read the file");
 
-    println!("Contents: {}", contents);
-
+    println!("Lines as vectors of floats: \n ======================================");
     for line in contents.lines() {
         let v: Vec<_> = line
             .split([','])
             .map(|char| char.parse::<f64>().unwrap())
             .collect();
-        println!("{:?}", v[0]);
+        println!("{:?}", v);
     }
+    let vec_of_vecs: Vec<Vec<_>> = contents
+        .lines()
+        .map(|line| {
+            line.split([','])
+                .map(|char| char.parse::<f64>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    println!("Vec of vecs: {:?}", vec_of_vecs);
+
+    let rows = vec_of_vecs.len();
+    let cols = vec_of_vecs[0].len();
+
+    let matrix = na::DMatrix::from_fn(rows, cols, |r, c| vec_of_vecs[r][c]);
+
+    println!("Our Matrix: {:?}", matrix);
+
+    assert_eq!(vec_of_vecs[1][2], matrix[(1, 2)]);
+
+    return matrix;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+pub mod io;
+
+use crate::io::load_csv;
+
+extern crate nalgebra as na;
+
+use na::DMatrix;
+
 fn main() {
-    println!("Hello, world!");
+    let my_mat = load_csv::print_string_contents("./data/YA04/val/W_6_R.csv");
 }
