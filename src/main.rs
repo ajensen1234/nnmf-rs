1118640e255e76ec6249345a5a8412bb406c5f11
Andrew Jensen <contact@ajensen.org>
2024-03-28 19:06:22 -0400

    Initializing W,H randomly for ALSA

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use alg::alsa::alternating_leastsq_nnmf;
 fn main() {
     let path = "./data/YA04/YA04_EMG_L.csv";
     let matrix = load_csv::load_csv_matrix(path);
-    let (w, h) = alternating_leastsq_nnmf(matrix.clone(), 2);
+    let (w, _h) = alternating_leastsq_nnmf(matrix.clone(), 2);
 
     println!("W : {:?}", w);
     //println!("H : {:?}", h);
