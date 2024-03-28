dcb3e62c9a3dfba17208cb83a260871bc4200844
Andrew Jensen <contact@ajensen.org>
2024-03-28 18:35:33 -0400

    Some testing commits and removing println

--- src/io/load_csv.rs
+++ src/io/load_csv.rs
@@ -1,41 +1,30 @@
-use std::env;
 use std::fs;
-use std::io;
 extern crate nalgebra as na;
 extern crate ndarray as np;
 use na::*;
 pub fn load_csv_matrix(fp: &str) -> DMatrix<f64> {
-    println!("We are in: {:?}", env::current_dir());
-    println!("File Name {}", fp);
     let contents = fs::read_to_string(fp).expect("We should have been able to read the file");
-
-    // println!("Lines as vectors of floats: \n ======================================");
-    for line in contents.lines() {
-        let v: Vec<_> = line
-            .split([','])
-            .map(|char| char.parse::<f64>().unwrap())
-            .collect();
-        //println!("{:?}", v);
-    }
     let vec_of_vecs: Vec<Vec<_>> = contents
         .lines()
         .map(|line| {
             line.split([','])
-                .map(|char| char.parse::<f64>().unwrap())
+                .map(|char| {
+                    if char.parse::<f64>().unwrap() < 0.0 {
+                        0.0
+                    } else {
+                        char.parse::<f64>().unwrap()
+                    }
+                })
                 .collect()
         })
         .collect();
-
-    // println!("Vec of vecs: {:?}", vec_of_vecs);
 
     let rows = vec_of_vecs.len();
     let cols = vec_of_vecs[0].len();
 
     let matrix = na::DMatrix::from_fn(rows, cols, |r, c| vec_of_vecs[r][c]);
 
-    // println!("Our Matrix: {:?}", matrix);
-
     assert_eq!(vec_of_vecs[1][2], matrix[(1, 2)]);
 
-    matrix
+    return matrix;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 pub mod alg;
 pub mod io;
-use crate::alg::alsa;
 use crate::io::load_csv;
 
 extern crate nalgebra as na;
 
 use alg::alsa::alternating_leastsq_nnmf;
-use na::DMatrix;
 
 fn main() {
     let path = "./data/YA04/YA04_EMG_L.csv";
     let matrix = load_csv::load_csv_matrix(path);
-    let (w, h) = alternating_leastsq_nnmf(matrix.clone(), 4);
+    let (w, h) = alternating_leastsq_nnmf(matrix.clone(), 2);
 
     println!("W : {:?}", w);
-    println!("H : {:?}", h);
+    //println!("H : {:?}", h);
 
     // println!("W times H : {:?}", w * h);
 
-    assert_eq!(matrix, w * h);
+    //assert_eq!(matrix, w * h);
 }
