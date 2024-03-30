ac71fcd1cf41c132e6a01750a2c2f72b4c7978d6
l <>
2024-03-30 17:20:25 -0400

    Write matrix IO mod

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod alg;
 pub mod io;
 pub mod synth_data;
 use crate::io::load_csv;
+use crate::io::write_csv::write_matrix_to_csv;
 
 extern crate nalgebra as na;
 
@@ -14,11 +15,11 @@ fn main() {
 
     // we want to create a test matrix
     let (W_test, h_test, EMG_test) =
-        synth_data::generate_test_data::generate_test_data(20, 7, 3, false);
+        synth_data::generate_test_data::generate_test_data(4, 4, 2, false);
     println!("W_test: {:?}", W_test);
     println!("h_test: {:?}", h_test);
 
-    let (w_est, h_est) = lee_seung_multiplicative_update_rule(EMG_test.clone(), 3);
+    let (w_est, h_est) = lee_seung_multiplicative_update_rule(EMG_test.clone(), 2);
 
     assert_eq!(W_test * h_test, w_est * h_est);
 }
